14163b9d1150b8823e9f9ac517aa05f0ea3c23db
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2026-04-18 14:53:20 +0700

    third commit

--- Modul7_DhiesnaZetaKurnia/Program.cs
+++ Modul7_DhiesnaZetaKurnia/Program.cs
@@ -3,6 +3,7 @@ class Program
 {
     public static void Main(String[] args)
     {
-        FilmFavorit_103022400132.ReadJSON();
+        //FilmFavorit_103022400132.ReadJSON();
+        Watchlist_103022400132.ReadJSON();
     }
 }
